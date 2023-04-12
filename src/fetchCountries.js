9c51eb39e949fd165c438c35f5d1036f4618fe8c
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-04-12 23:09:19 +0300

    add part solution

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+const fields = 'fields=name,capital,population,flags,languages';
+
+function fetchCountries(name) {
+  return fetch(`${BASE_URL}/name/${name}?${fields}`).then(response => response.json());
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', debounce(onCurrentInput, DEBOUNCE_DELAY));
+// refs.input.addEventListener('input', onCurrentInput);
+
+function onCurrentInput(event) {
+  event.preventDefault();
+  const dataInput = event.target;
+  const inputValue = event.target.value.trim();
+
+  fetchCountries(inputValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+      } else if (countries.length > 2 && countries.length <= 10) {
+        const markup = countries.reduce((markup, country) => markup + listCountries(country), '');
+        updateListCountries(markup);
+      }
+      console.log(countries);
+    })
+    .catch(onFetchError);
+}
+
+function onFetchError(error) {
+  if (error.status === 404) {
+    Notify.failure('Oops, there is no country with that name');
+  }
+  console.log(error.status);
+}
+
+function listCountries({ flags, name }) {
+  return `<li class="country__item">
+            <img class="country__image" src="${flags.svg}" alt="${
+    flags.alt || `The flag of ${name.official}`
+  }"/>
+            <p class="country__text"> ${name.common}</p>
+        </li>`;
+}
+
+function updateListCountries(markup) {
+  refs.list.innerHTML = markup;
+}
+
+function renderCountriesCard() {}
